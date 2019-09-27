44c7d3592335c236d090454c4765f8dca4137ce6
Ezequiel Brilhante <ezequielrb7@gmail.com>
2019-09-27 15:22:06 -0300

    update das aulas

--- src/componentes/DoisComponentes.jsx
+++ src/componentes/DoisComponentes.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 
-const CompA = props =>
+export const CompA = props =>
     <h1>Primeiro diz: {props.valor}</h1>
 
-const CompB = props =>
+export const CompB = props =>
     <h1>Segundo diz: {props.valor}</h1>
 
-export { CompA, CompB }
+
+//export default function() {
+//    return <p>teste</p>
+//}
+
+//export { CompA, CompB }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-//import PrimeiroComponente from './componentes/PrimeiroComponente'
-//import { CompA, CompB } from './componentes/DoisComponentes'
-import MultiElementos from './componentes/MultiElementos'
+// import PrimeiroComponente from './componentes/PrimeiroComponente'
+// import { CompA, CompB } from './componentes/DoisComponentes'
+// import MultiElementos from './componentes/MultiElementos'
+// import FamiliaSilva from './componentes/FamiliaSilva'
+import Familia from './componentes/Familia'
+import Membro from './componentes/Membro'
 
 
 const elemento = document.getElementById('root')
 ReactDOM.render(
     <div>
-        <MultiElementos />
-        {/*<CompA valor="Olá, eu sou o componente A"/>
-        <CompB valor="B na área"/>*/}
-        {/*<PrimeiroComponente valor = "Bom dia"/>*/}
+        <Familia sobrenome="Pereira">
+            <Membro nome="Andre" />
+            <Membro nome="Mariana" />
+        </Familia>
+        {/* <FamiliaSilva /> */}
+        {/* <MultiElementos /> */}
+        {/* <CompA valor="Olá, eu sou o componente A"/>
+            <CompB valor="B na área"/> */}
+        {/* <PrimeiroComponente valor = "Bom dia"/> */}
     </div>
     , elemento)
 
